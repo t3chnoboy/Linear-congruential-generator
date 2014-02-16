79702833509a7eb43f370045b764f55eed510e6a
Dmitry Mazuro <dmitry.mazuro@icloud.com>
2014-02-17 02:12:38 +0300

    calculated min, max, mx, dx and period

--- lcg.hs
+++ lcg.hs
@@ -1,8 +1,25 @@
-r0 = 107839
-a = 18221
-m = 510300
+import Data.List
+import Data.Fixed
 
-rnd = r0 : [ (r * a) `mod` m | r <- rnd]
+r0 = 107839
+a  = 18221
+m  = 510300
+
+rnd = r0 : [ (r * a) `mod'` m | r <- rnd]
+
+unique = nub $ take 5000 rnd
+minR = minimum unique
+maxR = maximum unique
+period = length unique
+points = map (/m) unique
+mx = sum points / genericLength points
+dx = (sum $ map (\x -> (x - mx)^2) points) / (genericLength points)
 
 main = do
-    print $ take 10 $ rnd
+    print minR
+    print maxR
+    print period
+    print $ take 5 rnd
+    print $ take 5 points
+    print mx
+    print dx
