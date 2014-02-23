f8f5e68a78a57c691afd2ad8d9e40f802faf8e5d
Dmitry Mazuro <dmitry.mazuro@icloud.com>
2014-02-24 01:56:04 +0300

    added histogram

--- lcg.hs
+++ lcg.hs
@@ -1,18 +1,19 @@
 import Data.List
 import Data.Fixed
 import IOUtils
+import Graphics.Histogram
 
-main = do
-    a <- promptInt "a>"
-    r0 <- promptInt "r0"
-    m <- promptInt "m"
-    let
-      rnd    = r0 : [ (r * a) `mod'` m | r <- rnd]
-      unique = takeWhile (/= head rnd) $ tail rnd
-      t      = length  unique
-      points = map (/m) unique
-      µ      = sum points / genericLength points
-      var    = (sum $ map ( \x -> (x - µ)^2 ) points) / (genericLength points)
-      σ      = sqrt var
+main = do a <- promptInt "a>"
+          r0 <- promptInt "r0"
+          m <- promptInt "m"
+          let
+            rnd    = r0 : [ (r * a) `mod'` m | r <- rnd]
+            unique = takeWhile (/= head rnd) $ tail rnd
+            t      = length  unique
+            points = map (/m) unique
+            µ      = sum points / genericLength points
+            var    = (sum $ map ( \x -> (x - µ)^2 ) points) / (genericLength points)
+            σ      = sqrt var
 
-    putStrLn . unlines. map prettyPrint $ [("T = ", fromIntegral t), ("µ = ", µ), ("σ = ", σ)]
+          putStrLn . unlines. map prettyPrint $ [("T = ", fromIntegral t), ("µ = ", µ), ("σ = ", σ)]
+          plot [] $ histogram binSturges points
