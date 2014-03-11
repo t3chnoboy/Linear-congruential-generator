f1d4f656fc3e3a21622e9acb03a730992584ff4e
Dmitry Mazuro <dmitry.mazuro@icloud.com>
2014-03-12 00:04:22 +0300

    refactoring

--- lcg.hs
+++ lcg.hs
@@ -3,16 +3,15 @@ import Data.Fixed
 import IOUtils
 import ASCIIHist
 
-main = do [a, r0, m] <- sequence [prompt "a>", prompt "r0", prompt "m"]
-          let
-            rnd    = r0 : [ (r * a) `mod'` m | r <- rnd]
-            rnd'   = tail rnd
-            unique = takeWhile (/= head rnd') $ tail rnd'
-            t      = length  unique
-            points = map (/m) unique
-            µ      = sum points / genericLength points
-            var    = (sum $ map ( \x -> (x - µ)^2 ) points) / (genericLength points)
-            σ      = sqrt var
+main = do [a, r0, m] <- sequence $ map prompt ["a>", "r0", "m"]
+          let rnd    = r0 : [ (r * a) `mod'` m | r <- rnd]
+              rnd'   = tail rnd
+              unique = takeWhile (/= head rnd') $ tail rnd'
+              t      = length  unique
+              points = map (/m) unique
+              µ      = sum points / genericLength points
+              var    = (sum $ map ( \x -> (x - µ)^2 ) points) / (genericLength points)
+              σ      = sqrt var
 
           putStrLn . unlines. map prettyPrint $ [("T = ", fromIntegral t), ("µ = ", µ), ("σ = ", σ)]
           putStr . unlines $ histogram points 20
